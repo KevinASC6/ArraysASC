4e48b627e1e846a8f528d2a458c35842c39065af
KevinASC6 <kpepin21@riverdale.edu>
2019-07-10 10:07:11 -0400

    first commit

---
+++ arrays.js
@@ -0,0 +1,21 @@
+// const heroes = [
+//     'Iron Man', 'Batman', 'Black Panther', 'Captain America', 'Spiderman'
+// ];
+// console.log('The length is' , heroes.length);
+
+// heroes.push('Captain Marvel');
+// console.log('The new  length is' , heroes.length);
+
+// heroes[1] = 'Nightwing';
+// console.log(heroes[1]);
+
+// heroes.pop();
+// console.log(heroes);
+
+const ballerz = ['Saquon', 'Odell', 'Baker', 'AB', 'Deshaun Watson'];
+ballerz.splice(1,0,'Julio');
+
+console.log(ballerz);
+
+ballerz.splice(3,2);
+console.log(ballerz);
